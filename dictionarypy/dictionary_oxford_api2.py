e8b6943cabae3f017882c01815913a7cea9ededc
Binh Vo <binhvo07@gmail.com>
2021-09-08 16:34:23 -0500

    still trying to implement to get all the definitions of a word

--- dictionarypy/dictionary_oxford_api2.py
+++ dictionarypy/dictionary_oxford_api2.py
@@ -10,17 +10,22 @@ endpoint = "entries"
 language_code ="en-us"
 word_id = input("Word: " )
 
+# Get request to url. Once the response returns, convert the response to unicode txt and json.loads() can be used to parse a valid Json string and
+# convert it to a Python dictionary
 url = "https://od-api.oxforddictionaries.com/api/v2/" + endpoint + "/" + language_code + "/" + word_id.lower()
 
 response = requests.get(url, headers={"app_id": app_id, "app_key": api_key})
 
 json_data = json.loads(response.text) # returns a python dictionary object with (response.text -> returns a response in unicode)
 
+
+# Get all
 definitions_arr = json_data["results"]
 i = 1
 for definition in definitions_arr:
-    meaning = definition["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
-    #print (type(meaning))
-    print(f"\nDefinition {i}: {meaning}")
-    i+=1
+  meanings = definition["lexicalEntries"][0]["entries"][0]["senses"]
+  for meaning in meanings:
+      word_definition = meaning['definitions'][0]
+      print(f"\n{i}. {word_definition}")   
+      i+=1
 #print(json_data)
