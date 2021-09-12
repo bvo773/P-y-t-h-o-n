c4f1885f6967f9822c744c73685bb35a6a827fbc
Binh Vo <binhvo07@gmail.com>
2021-09-11 21:08:43 -0500

    bible api can search up through most of the old testament by book and chapters

--- miscpy/bible.py
+++ miscpy/bible.py
@@ -16,21 +16,40 @@ def get_book_prefix(book):
   elif (book == "numbers"): prefix = "num"
   elif (book == "deuteronomy"): prefix = "deu"
   elif (book == "joshua"): prefix = "joshua"
-  else: prefix = "wrong input for book"
+  elif (book == "1 samuel"): prefix = "1sam"
+  elif (book == "2 samuel"): prefix = "2sam"
+  elif (book == "ruth"): prefix = "ruth"
+  elif (book == "1 kings"): prefix ="1kings"
+  elif (book == "2 kings"): prefix ="2kings"
+  elif (book == "1 chronicles"): prefix ="1chronicles"
+  elif (book == "2 chronicles"): prefix ="2chronicles"
+  elif (book == "ezra"): prefix ="ezra"
+  elif (book == "nehemiah"): prefix ="nehemiah" 
+  elif (book == "esther"): prefix ="esther"
+  elif (book == "job"): prefix ="job"
+  elif (book == "psalm"): prefix ="ps"
+  elif (book == "1 kings"): prefix ="1kings"
+  else: 
+    prefix = "wrong input for book"
   
   return prefix
 
-def passage(book, chapter):
+def get_book(book):
+  url = f"https://getbible.net/json?passage={book}&raw=true"
+  response = requests.get(url)
+  data = json.loads(response.text)
+  chapters = data["book"]
+  print(f"There are {len(chapters)} chapters. Choose a chapter between 1 - {len(chapters)}")
+
+def get_passage(book, chapter):
   url = f"http://getbible.net/json?passage={book}{chapter}&raw=true"
   print((url))
   
   response = requests.get(url)
   jdata = json.loads(response.text)
   #print(response.status_code, response.headers)
-  #print(jdata)
   
   chapter = jdata["chapter"]
-  
   
   for line in chapter.items():
     linenum = line[0]
@@ -38,16 +57,55 @@ def passage(book, chapter):
     verse = linedict["verse"]
     print(linenum, verse)
  
+def menu():
+  print("\nBIBLE MENU OPTIONS")
+  print("Press 1 - Search for a book")
+  print("Press 2 - Search for a passage")
+  choice = str(input("My Option: "))
+  
+  if (choice == "1"): 
+    book = str(input("What book would you like me to open? (eg. Genesis): "))  
+    prefix = get_book_prefix(book.lower()) 
+    get_book(prefix)
+    
+    chapter = str(input("What chapter would you like me to open?: "))
+    get_passage(prefix, chapter)
 
-def main():
+  elif (choice == "2"):
+    book = str(input("Book (eg. Matthew): ")) 
+    prefix = get_book_prefix(book.lower())
+    get_book(prefix)
+    
+    chapter = str(input("What chapter do you want to look at?: "))
+    get_passage(prefix, chapter)
   
-  book = str(input("Book: ")) 
-  chapter = str(input("Chapter: "))
+  else:
+    print("Invalid choice")
+
+def terminate_loop():
+  answer = str(input("Continue (Y/N)?: "))
+  terminate = False
+  if (answer == 'Y' or answer == 'y'):
+    terminate = True
+  elif (answer == 'N' or answer == 'n'):
+    terminate = False
   
+  return terminate
+     
+def main(): 
   
-  passage(get_book_prefix(book.lower()), chapter)
-  
+  #get_passage(get_book_prefix(book.lower()), chapter)
+  loop = True
+  try:
+    while(loop):
+      menu()
+      loop = terminate_loop()
+    print("Hope you enjoy the word. bless u!")
+  except KeyError:
+    print("Wrong spelling for inputs")
+  except ValueError:
+    print("Failed to decode json")
 
 if __name__ == '__main__':
   main()
-
+  
